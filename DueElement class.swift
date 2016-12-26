3d87c6d2dd588c0730cfbcec52958a2359a9bd46
Xintong Wu <xwu68@illinois.edu>
2016-12-26 13:31:34 -0600

    Add files via upload

---
+++ DueElement class.swift
@@ -0,0 +1,24 @@
+// func foo(para: Int) -> Int
+// {
+//   return 0
+// }
+//
+// var list:[DueElement]
+// list.append()
+// print("hello world this is \(myName)")
+
+class DueElement{
+  //content
+  var text?:string;
+  var duedate?;
+
+  //helper variable
+  var dueinterval?;
+  var color?;
+  var createdate?;
+
+  //reference
+  let setToYellow = 2; //2days
+  let setToRed = 1; //1days left
+
+}
