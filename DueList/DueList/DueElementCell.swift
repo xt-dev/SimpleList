f41ff43c75af2c4e332d839c4decf9f0c527ad3b
Xintong Wu <xwu68@illinois.edu>
2016-12-26 14:56:53 -0600

    Xcode

---
+++ DueList/DueList/DueElementCell.swift
@@ -0,0 +1,15 @@
+//
+//  DueElementCell.swift
+//  DueList
+//
+//  Created by Derek Wu on 2016/12/24.
+//  Copyright © 2016年 Xintong Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DueElementCell: UITableViewCell{
+    
+    
+}
