3d960bc25b211ae5d795ead13c51b433c737151d
Xintong Wu <xwu68@illinois.edu>
2016-12-31 16:39:53 -0600

    SimpleList_noError!

---
+++ A_Simple_List/A_Simple_List/DueElementCell.swift
@@ -0,0 +1,18 @@
+//
+//  DueElementCell.swift
+//  A_Simple_List
+//
+//  Created by Derek Wu on 2016/12/31.
+//  Copyright © 2016年 Xintong Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DueElementCell: UITableViewCell{
+    @IBOutlet weak var DueDateLabel: UILabel!
+    @IBOutlet weak var DueNameLabel: UILabel!
+    @IBOutlet weak var TimeLeftLabel: UILabel!
+    @IBOutlet weak var ProgressBar: UIProgressView!
+    
+}
---
+++ A_Simple_List/A_Simple_List/InputViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InputViewController.swift
+//  A_Simple_List
+//
+//  Created by Derek Wu on 2016/12/31.
+//  Copyright © 2016年 Xintong Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InputViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Checkpoint")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
