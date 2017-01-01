cc3b94b9c58f1c6ad458ae69de682068d9c1dcf8
rzhang74 <ruilinzhang1997@gmail.com>
2016-12-31 23:47:50 -0600

    add Delete Done animation; add UIColor extension

---
+++ A_Simple_List RuilinZhang/A_Simple_List/DueElementCell.swift
@@ -0,0 +1,19 @@
+//
+//  DueElementCell.swift
+//  A_Simple_List
+//
+//  Created by Derek Wu on 2016/12/31.
+//  Copyright © 2016年 Xintong Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MCSwipeTableViewCell
+
+class DueElementCell: MCSwipeTableViewCell{
+    @IBOutlet weak var DueDateLabel: UILabel!
+    @IBOutlet weak var DueNameLabel: UILabel!
+    @IBOutlet weak var TimeLeftLabel: UILabel!
+    @IBOutlet weak var ProgressBar: UIProgressView!
+    
+}
