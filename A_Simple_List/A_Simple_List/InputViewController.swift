25517720835cb052f875dbc93844e5cc0f7996a7
Xintong Wu <xwu68@illinois.edu>
2017-01-03 15:23:46 -0600

    1.3 Update

--- A_Simple_List/A_Simple_List/InputViewController.swift
+++ A_Simple_List/A_Simple_List/InputViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 class InputViewController: UIViewController{
+    @IBOutlet weak var InputTextField: UITextField!
     
     //TODO: Handle swipe with larger area
     
@@ -17,11 +18,23 @@ class InputViewController: UIViewController{
         if(sender.direction == .up){
             print("check!")
             let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "LVC")
-            self.present(secondViewController!, animated: true, completion: nil)
+            self.present(secondViewController!, animated: false, completion: nil)
         }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    
+    //Draw the bottom line for the textfield
+    override func viewDidLayoutSubviews() {
+        let border = CALayer()
+        let width = CGFloat(2.0)
+        border.borderColor = UIColor.init(netHex:0x9B9B9B, isLargerAlpha: 0.7).cgColor
+        border.frame = CGRect(x: 0, y: InputTextField.frame.size.height - width, width:  InputTextField.frame.size.width, height: InputTextField.frame.size.height)
+        
+        border.borderWidth = width
+        InputTextField.layer.addSublayer(border)
+        InputTextField.layer.masksToBounds = true
     }
     
     override func viewDidLoad() {
--- A_Simple_List/A_Simple_List/ListViewController.swift
+++ A_Simple_List/A_Simple_List/ListViewController.swift
@@ -16,7 +16,6 @@ var dueList = [DueElement(dueName: "CS225 MP", dueDate: time(year: 2017, month: 
 //var customView: UIView!
 
 
-
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var DueListView: UITableView!
@@ -30,16 +29,85 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     var viewTransitionManager = ViewTransitionManager()
     
+    //Pan Gesture
+    func createPanGestureRecognizer(targetView: UIView)
+    {
+        let panGesture = UIPanGestureRecognizer(target: self, action:#selector(self.handlePanGesture(panGesture:)))
+        targetView.addGestureRecognizer(panGesture)
+    }
+    
+    func gestureRecognizerShouldBegin(gestureRecognizer: UIPanGestureRecognizer) -> Bool {
+        let translation = gestureRecognizer.translation(in: view)
+        let zero: CGPoint = CGPoint(x: 0, y: 0)
+        gestureRecognizer.setTranslation(zero, in: view)
+        let ref: CGFloat? = 30
+        if (translation.y > ref!) { return true}
+        else {return false}
+    }
+    
+    func handlePanGesture(panGesture: UIPanGestureRecognizer) {
+        
+        //        get translation
+        
+        let translation = panGesture.translation(in: view)
+//        if translation.dictionaryRepresentation = 
+        let zero: CGPoint = CGPoint(x: 0, y: 0)
+        panGesture.setTranslation(zero, in: view)
+        //let x_loc:CGFloat? = translation.x
+        let y_loc:CGFloat? = translation.y
+        let ref : CGFloat? = 30
+        print(translation.y)
+        if (y_loc! > ref!)
+        {
+            print(">60")
+            let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "IVC")
+            secondViewController?.transitioningDelegate = self.viewTransitionManager
+            //self.view.isHidden = true
+            self.present(secondViewController!, animated: true, completion: nil)
+        }
+        //create a new Label and give it the parameters of the old one
+        //var label = panGesture.view as! UITableView
+        //label.center = CGPoint(x: label.center.x+translation.x, y: label.center.y+translation.y)
+        //label.isMultipleTouchEnabled = true
+        //label.isUserInteractionEnabled = true
+        
+        if panGesture.state == UIGestureRecognizerState.began {
+            
+            //add something you want to happen when the Label Panning has started
+            print("Begin")
+        }
+        
+        if panGesture.state == UIGestureRecognizerState.ended {
+            if (translation.y > 10) { print("?10")}
+            //add something you want to happen when the Label Panning has ended
+            
+        }
+        
+        
+        if panGesture.state == UIGestureRecognizerState.changed {
+            
+            //add something you want to happen when the Label Panning has been change ( during the moving/panning ) 
+            
+        }
+            
+        else {
+            
+            // or something when its not moving
+        }
+        
+    }
     
 //TODO: Try to use refreshcontent to triger IPV
     
 //    func loadCustomRefreshContents() {
-//        let refreshContents = Bundle.main.loadNibNamed("InputView", owner: self, options: nil)
-//        customView = refreshContents?[0] as! UIView
+//        let refreshContents = Bundle.main.loadNibNamed("InputViewController", owner: self, options: nil)
+        //customView = refreshContents?[0] as! UIView
+//        customView = DueListView.backgroundView
 //        customView.frame = refreshControl.bounds
 //        refreshControl.addSubview(customView)
 //    }
     
+    //Swipe Gestures
     func handleSwipes(_ sender : UISwipeGestureRecognizer){
         if(sender.direction == .down /*&& sender.location(ofTouch: <#T##Int#>, in: <#T##UIView?#>)*/){
             print("check!")
@@ -48,6 +116,8 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
             self.present(secondViewController!, animated: false, completion: nil)
         }
     }
+
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let InputViewController = segue.destination as! InputViewController
@@ -82,19 +152,19 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.separatorInset = UIEdgeInsets.zero
         cell.layoutMargins = UIEdgeInsets.zero
         cell.selectionStyle = .none
-        cell.defaultColor = UIColor(netHex:0x1ABC9C, isLargerAlpha: false)
+        cell.defaultColor = UIColor(netHex:0x1ABC9C, isLargerAlpha: 1)
         cell.firstTrigger = 0.25;
         cell.secondTrigger = 0.50;
         
         
         //add Listener
-        cell.setSwipeGestureWith(UIImageView(image: UIImage(named: "check")!), color: UIColor(netHex:0x1ABC9C, isLargerAlpha: true), mode: .exit, state: .state1, completionBlock: { (cell, state, mode) -> Void in
+        cell.setSwipeGestureWith(UIImageView(image: UIImage(named: "check")!), color: UIColor(netHex:0x1ABC9C, isLargerAlpha: 1), mode: .exit, state: .state1, completionBlock: { (cell, state, mode) -> Void in
             
             dueList.remove(at: indexPath.row)
             self.DueListView.reloadData()
             
         })
-        cell.setSwipeGestureWith(UIImageView(image: UIImage(named: "cross")!), color:  UIColor(netHex:0xEC644B, isLargerAlpha: true), mode: .exit, state: .state3, completionBlock: { (cell, state, mode) -> Void in
+        cell.setSwipeGestureWith(UIImageView(image: UIImage(named: "cross")!), color:  UIColor(netHex:0xEC644B, isLargerAlpha: 1), mode: .exit, state: .state3, completionBlock: { (cell, state, mode) -> Void in
             dueList.remove(at: indexPath.row)
             self.DueListView.reloadData()
         })
@@ -103,7 +173,6 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         return(cell)
     }
     
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -111,7 +180,18 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.handleSwipes(_:)))
         swipeDown.direction = .down
         view.addGestureRecognizer(swipeDown)
-        DueListView.addGestureRecognizer(swipeDown)
+        //DueListView.addGestureRecognizer(swipeDown)
+        
+        createPanGestureRecognizer(targetView: self.DueListView)
+        
+//        refreshControl = UIRefreshControl()
+//        refreshControl.
+//        DueListView.addSubview(refreshControl)
+//        if (refreshControl.isRefreshing == true)
+//        {
+//            print("refresh")
+//        }
+//        loadCustomRefreshContents()
     }
 
     override func didReceiveMemoryWarning() {
@@ -124,20 +204,16 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
 
 
 extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int, isLargerAlpha: Bool) {
+    convenience init(red: Int, green: Int, blue: Int, isLargerAlpha: Float) {
         assert(red >= 0 && red <= 255, "Invalid red component")
         assert(green >= 0 && green <= 255, "Invalid green component")
         assert(blue >= 0 && blue <= 255, "Invalid blue component")
         
-        if(isLargerAlpha==true){
-            self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 0.9)
-        }else{
-            self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 0.7)
-        }
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: CGFloat(isLargerAlpha))
+        
     }
     
-    convenience init(netHex:Int, isLargerAlpha: Bool) {
+    convenience init(netHex:Int, isLargerAlpha: Float) {
         self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff, isLargerAlpha: isLargerAlpha)
     }
 }
-
